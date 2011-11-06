f80f2a529c67be6a0cb88a4d1b9269054705e68b
Scott Smith <scott@disqus.com>
2011-11-06 01:10:00 -0800

    cleanup to pass jshint.com

--- grockets.js
+++ grockets.js
@@ -1,9 +1,11 @@
-var io = require('socket.io').listen(8000)
-  , http = require('http');
+var io = require('socket.io').listen(8000),
+  http = require('http');
 
-var graphiteHost = 'CHANGE-ME-YOU-NOOB';
+var graphiteHost = 'CHANGE-ME-YOU-NOOB',
+  handlers = [];
 
 function buildQueryString(parameters) {
+  'use strict';
   var queryParameters = [];
 
   parameters.targets.forEach(function(target) {
@@ -17,6 +19,7 @@ function buildQueryString(parameters) {
 }
 
 function processHandlers() {
+  'use strict';
   handlers.map(function(handler) {
     if (handler.shouldUpdate()) {
       handler.data(function(data) {
@@ -29,9 +32,10 @@ function processHandlers() {
 }
 
 function MetricHandler(id, socket, targets, from, updateInterval) {
-  var from = typeof(from) != 'undefined' ? from : '-24h',
+  'use strict';
+  var from = typeof(from) !== 'undefined' ? from : '-24h',
     lastUpdateTime = 0,
-    updateInterval = typeof(updateInterval) != 'undefined' ? updateInterval : 5000,
+    updateInterval = typeof(updateInterval) !== 'undefined' ? updateInterval : 5000,
     queryParameters = {
       'targets': targets,
       'from': from
@@ -42,12 +46,12 @@ function MetricHandler(id, socket, targets, from, updateInterval) {
 
   this.addTarget = function(target) {
     queryParameters.targets.push(target);
-  }
+  };
 
   this.removeTarget = function(target) {
     var index = queryParameters.targets.indexOf(target);
     queryParameters.targets.splice(index, 1);
-  }
+  };
 
   this.data = function(callback) {
     var httpOptions = {
@@ -78,27 +82,26 @@ function MetricHandler(id, socket, targets, from, updateInterval) {
     } else {
       return(false);
     }
-  }
+  };
 }
 
-var handlers = [];
-
 io.sockets.on('connection', function(socket) {
+  'use strict';
   socket.on('fetchMetrics', function(queryData) {
     handlers.push(new MetricHandler(queryData.id, socket, queryData.targets, queryData.from, queryData.updateInterval));
   }).on('addTarget', function(target) {
     handlers.forEach(function(handler) {
-      if (handler.socket == socket) {
+      if (handler.socket === socket) {
         handler.addTarget(target);
       }
     });
   }).on('removeTarget', function(target) {
     handlers.forEach(function(handler) {
-      if (handler.socket == socket) {
+      if (handler.socket === socket) {
         handler.removeTarget(target);
       }
     });
-  });;
+  });
 });
 
 processHandlers();
